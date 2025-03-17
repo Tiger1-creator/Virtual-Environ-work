058388b9e1c1d9cad75bca14f3c14e4a76155fd0
Tiger1-creator <olivierniyongabo200@gmail.com>
2025-03-17 16:20:19 +0000

    version 3

--- pyfun.py
+++ pyfun.py
@@ -1,6 +1,19 @@
 # text = input("enter string you want:")
 # print(len(text))
 
-text = input("enter string you want:")
-text=text.replace(" ","")
-print(len(text))
+# text = input("enter string you want:")
+# text=text.replace(" ","")
+# print(len(text))
+
+
+a = input("enter first string A: ")
+b = input("enter second string B: ")
+c = input("enter third string C: ")
+d = input("enter forth string D: ")
+e= input("enter fifth string E: ")
+a = a.replace(" ", "")
+b = b.replace(" ", "")
+c = c.replace(" ", "")
+d = d.replace(" ", "")
+e = e.replace(" ", "")
+print("length of A= ", len(a), "length of B= ", len(b), "length of C= ", len(c), "length of D= ", len(d), "length of E= ", len(e))
